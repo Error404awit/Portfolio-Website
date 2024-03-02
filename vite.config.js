365e9adcf9f479dd13c7591be827a8d743039aa6
unknown <bmode5483@gmail.com>
2024-03-02 10:14:38 +0800

    Portfolio Website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/Portfolio-Website/", //Para ma deploy sa github pages and you need to install npm install gh-pages tas punta ka sa
+  plugins: [react()], //package.json
+});
