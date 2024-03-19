7bfe40f1277870812d54c0cad518e7896fd78b0d
unknown <bmode5483@gmail.com>
2024-03-19 15:54:01 +0800

    Added ..

--- src/Components/SliderData.jsx
+++ src/Components/SliderData.jsx
@@ -1,8 +1,8 @@
 export const SliderData = [
   { image: "../src/assets/html.png" },
-  { image: "./src/assets/css.png" },
-  { image: "./src/assets/javascript.png" },
-  { image: "./src/assets/react.png" },
-  { image: "./src/assets/tailwind.png" },
-  { image: "./src/assets/github.png" },
+  { image: "../src/assets/css.png" },
+  { image: "../src/assets/javascript.png" },
+  { image: "../src/assets/react.png" },
+  { image: "../src/assets/tailwind.png" },
+  { image: "../src/assets/github.png" },
 ];
